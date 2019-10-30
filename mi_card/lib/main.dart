7cf87f97859c98d239719ab3a8e2ecb564e40f23
Jonathan <jlemanski1@gmail.com>
2019-10-30 17:23:05 -0600

    Create empty Flutter Application

---
+++ mi_card/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyCardApp());
+}
+
+class MyCardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    );
+  }
+}
