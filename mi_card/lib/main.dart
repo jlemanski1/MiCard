51ca568f3e47bda374b2e348f5a23a0771716fd3
Jonathan <jlemanski1@gmail.com>
2019-10-30 20:45:47 -0600

    Added Avatar Image and name for start of card layout

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MiCardApp());
 }
-  
+
 
 class MiCardApp extends StatelessWidget {
   @override
@@ -11,6 +11,26 @@ class MiCardApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                // Placeholder, will be changeable from settings later
+                backgroundImage: AssetImage('images/Jonathan.jpg'),
+              ),
+              Text(
+                'Jonathan Lemanski',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+
+            ],
+          ),
+        ),
       ),
     );
   }
