9035ef77113a87dc499af1c2344bb5d2708254dd
Jonathan <jlemanski1@gmail.com>
2019-11-01 20:52:05 -0600

    Refactored app to StatefulWidget so that fields can be changed

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -4,102 +4,127 @@ void main() {
   runApp(MiCardApp());
 }
 
+class MiCardApp extends StatefulWidget {
+  @override
+  MiCardAppState createState() => MiCardAppState();
+}
 
-class MiCardApp extends StatelessWidget {
+class MiCardAppState extends State<MiCardApp> {
+
+  @override
+  void initState() {
+
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                // Placeholder, will be changeable from settings later
-                backgroundImage: AssetImage('images/Jonathan.jpg'),
-              ),
-              Text(
-                'Jonathan Lemanski',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'Mobile App Developer',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal[100],
-                  letterSpacing: 2.5,            
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal[100],
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              // Placeholder, will be changeable from settings later
+              backgroundImage: AssetImage('images/Jonathan.jpg'),
+            ),
+            Text(
+              'Jonathan Lemanski',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
                 color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+1 780 000 000',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                      color: Colors.teal[900]
-                    ),
+              ),
+            ),
+            Text(
+              'Mobile App Developer',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontWeight: FontWeight.bold,
+                color: Colors.teal[100],
+                letterSpacing: 2.5,            
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+1 000 000-0000',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.teal[900]
                   ),
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'email@address.com',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 18.0,
+                    color: Colors.teal[900],
                   ),
-                  title: Text(
-                    'email@address.com',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                      color: Colors.teal[900],
-                    ),
-                  ),
+                
                 ),
               ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.web,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'Portfolio Website',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                      color: Colors.teal[900],
-                    ),
-                  ),                 
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.web,
+                  color: Colors.teal,
                 ),
+                title: Text(
+                  'portfolio.com',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.teal[900],
+                  ),
+                ),                 
               ),
-            ],
-          ),
+            ),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              alignment: Alignment(1.0, 1.0),
+              child: FloatingActionButton(
+                backgroundColor: Colors.teal,
+                child: Icon(
+                  Icons.settings,
+                  color: Colors.teal[900],
+                  ),
+                onPressed: () {
+
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
